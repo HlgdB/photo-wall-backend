1c39b3ec155b7ee4b340c202129b6f20aba7b56c
HlgdB <1297693955@qq.com>
2022-01-26 01:13:51 +0800

    feat: tag system step01

---
+++ index.js
@@ -0,0 +1,50 @@
+var handle = require('./requestHandler');
+const path = require('path');
+var express = require('express');
+var multer = require('multer');
+var app = express();
+var upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      // 上传文件存在 uploads 下
+      cb(null, 'uploads/')
+    },
+    filename(req, file, cb) {
+      // 使用时间戳作为上传的文件名
+      const extname = path.extname(file.originalname)
+      console.log(extname);
+      cb(null, Date.now() + extname)
+    }
+  })
+});
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+app.get('/getAllInfos', function(req, res) {
+  res.send(handle.readAlbumTagInfos());
+});
+
+app.post('/upload', upload.array('photos', 9), function(req, res, next){
+  // 上传好后到文件信息在req.files里
+  // console.log(req.files);
+  // 上传附带的文本域信息在req.body里
+  // console.log(req.body);
+
+  res.json({
+    ok: true,
+    message: '图片上传成功'
+  })
+})
+
+var server = app.listen(8888, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
---
+++ requestHandler.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+function readAlbumTagInfos() {
+  try {
+    const data = fs.readFileSync("./albumTagInfos.json", "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(`Error reading file from disk: ${err}`);
+  }
+}
+
+exports.readAlbumTagInfos = readAlbumTagInfos;
